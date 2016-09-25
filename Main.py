a02fc43aa7b00b4c452d7fb75679ce53b7328a69
Jeremy Daugherty <Jeremy.Daug@gmail.com>
2016-09-25 11:41:17 -0500

    2nd to last update. Will be reworking the state to use the minimal data., that should hopefully speed it up.

--- Main.py
+++ Main.py
@@ -27,44 +27,29 @@ def main(argv):
     solved = False
     dirs = [UP, DOWN, LEFT, RIGHT]
 
-    # We set a catch ammount equal to twice the size of the entire grid,
-    # this is a just in case
-    # measure to ensure the program doesn't loop indefinitely.
-    hard_max = Puzzle.Head.height * Puzzle.Head.width * 2
     while not solved:
         # find the best option from the frontier.
         Puzzle.Active = Puzzle.get_best()
         # if the steps are higher then our hard_max, then move on.
-        if Puzzle.Active.step_count() <= hard_max:
-            # print(Puzzle.Active.steps)
-            # check if our current state is the goal
-            if Puzzle.check_soln():
-                # if it is, mark it and break out of this popsicle stand.
-                solved = True
-                break
-            # if it's not, then put it in the explored set
-            Puzzle.explored.append(Puzzle.Active)
-            # run through all the directions it
-            # could go and add them to the frontier.
-            for i in dirs:
-                # get the next state
-                temp_state = Puzzle.move(i)
-                # If the move succeded (returned not None) do more.
-                if temp_state is not None:
-                    # If it's not an explored state, add it to the frontier.
-                    if temp_state not in Puzzle.explored:
-                        Puzzle.frontier.append(temp_state)
+        # print(Puzzle.Active.steps)
+        # check if our current state is the goal
+        if Puzzle.check_soln():
+            # if it is, mark it and break out of this popsicle stand.
+            solved = True
+            break
+        # if it's not, then put it in the explored set
+        Puzzle.explored.append(Puzzle.Active)
+        # run through all the directions it
+        # could go and add them to the frontier.
+        for i in dirs:
+            # get the next state
+            temp_state = Puzzle.move(i)
+            # If the move succeded (returned not None) do more.
+            if temp_state is not None:
+                # If it's not an explored state, add it to the frontier.
+                if temp_state not in Puzzle.explored:
+                    Puzzle.frontier.append(temp_state)
                     # if it's in the explored do nothing and keep going on.
-        elif Puzzle.frontier:
-            # if we somehow have a state over our hard_max and
-            # our frontier has more in it
-            # then move on.
-            continue
-        else:
-            # finally if our max step is larger then our hard
-            # limit, and the frontier is
-            # empty then stop running and output our failure.
-            print("Uh Oh, we couldn't find an answer.")
 
     end = time()
     if solved:
